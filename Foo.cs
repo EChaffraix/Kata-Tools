a73493efd5f2d32c5d4a2bc9f5cca15478f08cc0
EChaffraix <emmanuel@chaffraix.fr>
2021-01-19 09:39:49 +0100

    Create .net project

---
+++ Foo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FakeProject.Tests
+{
+    [TestFixture]
+    public class Foo
+    {
+        [Test]
+        public void Bar()
+	{
+	    int i = 41;
+	    i++;
+	    Assert.That(i, Is.EqualTo(42));
+	}
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(40)]
+        public void FooBar(int i)
+	{
+	    i++;
+	    Assert.That(i, Is.LessThan(42));
+	}
+
+    }
+}
--- create_dotnet_project.sh
+++ create_dotnet_project.sh
@@ -7,21 +7,27 @@ project_name=$1
 tests_name=$project_name.Tests
 sln=$project_name.sln
 
+framework=netcoreapp2.2
+tools_dir=/c/dev/Kata-Tools/
+
 mkdir -p $project_name/src
 cd $project_name/src
-dotnet new console -n $project_name
+dotnet new console -n $project_name -f $framework
 cd ..
 
 mkdir tests
 cd tests
-dotnet new classlib -n $tests_name --framework netcoreapp2.0
+dotnet new nunit -n $tests_name -f $framework
 cd $tests_name
-dotnet add $tests_name.csproj package Nunit
+#dotnet add $tests_name.csproj package Nunit
 dotnet add $tests_name.csproj package NSubstitute
-dotnet add $tests_name.csproj package Microsoft.NET.Test.Sdk
-dotnet add $tests_name.csproj package NUnit3TestAdapter
+#dotnet add $tests_name.csproj package Microsoft.NET.Test.Sdk
+#dotnet add $tests_name.csproj package NUnit3TestAdapter
 #dotnet add $tests_name.csproj package 
 dotnet add $tests_name.csproj reference ../../src/$project_name/$project_name.csproj
+rm UnitTest1.cs
+cp $tools_dir/Foo.cs .
+sed -i "s/namespace.*/namespace $tests_name/" Foo.cs
 cd ../..
 
 dotnet new sln -n $project_name
